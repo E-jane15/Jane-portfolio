e4fb307666c1b363fa358a513c7d8853d4e2fa06
E-jane15 <janeebaimay@gmail.com>
2023-06-06 19:05:38 +0100

    footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import "./footer.css";
+import { BsInstagram } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
 
-export default Footer
+      <div className="footer__socials">
+        <a href="https://www.instagram.com/jane_ebai/" target="_blank">
+          <BsInstagram />
+        </a>
+        <a href="https://twitter.com/Jane Ebai" target="_blank">
+          <BsTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Jane Ebai All rights reserved</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
