d5c30aa8af7bb73bd44c74c612b133ed515dbb61
E-jane15 <janeebaimay@gmail.com>
2023-06-06 18:42:34 +0100

    header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/jane.png";
+import HeaderSocials from "./HeaderSocials";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="container header__container">
+        <h5>Hello I am</h5>
+        <h1>Ebai Enownku Jane</h1>
+        <h5 className="text-light">
+          {" "}
+          Software Engineering Student,Data Analyst and Web developer
+        </h5>
+        <CTA />
+        <HeaderSocials />
 
-export default Header
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
