0c235d47d64eb3963a381d242809a9327737905c
E-jane15 <janeebaimay@gmail.com>
2023-06-06 19:04:11 +0100

    contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React from "react";
+import "./contact.css";
+import { TfiEmail } from "react-icons/tfi";
+import { BsInstagram } from "react-icons/bs";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const form = useRef();
 
-export default Contact
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_zw79eef",
+      "template_v2zk59f",
+      form.current,
+      "s9LVVssyQ-S_QYygN"
+    );
+
+    e.target.reset();
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get In touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <TfiEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>janeebaimay@gmail.com</h5>
+            <a href="mailto:janeebaimay@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <BsInstagram className="contact__option-icon" />
+            <h4>Instagram</h4>
+            <h5>Jane</h5>
+            <a href="https://ig.me/jane_ebai" target="_blank">
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <IoLogoWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+237 682 735 859</h5>
+            <a href="https://wa.me/message/CK6QHV5DUBM3H1" target="_blank">
+              Send a message
+            </a>
+          </article>
+        </div>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="text" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
